6cf35bbbe9fa61cbd3f494f163d67d35f1c84990
mukesh-pasi <119421596+mukesh-pasi@users.noreply.github.com>
2022-12-01 12:54:14 +0530

    Added poc soln

---
+++ New folder/TestProject1/Lola.Hooks/DataSetup.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium.DevTools.V105.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Configuration;
+
+namespace TestProject1.Lola.Hooks
+{
+    [Binding]
+    public class DataSetup
+    {
+       
+        [BeforeScenario]
+        public void GetLocalData()
+        {
+            
+        }
+        
+    }
+}
